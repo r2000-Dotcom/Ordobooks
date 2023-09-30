d405c221b105dfd7f02a2269c091d04752818835
Rupak Shukla <rupakshukla4@gmail.com>
2023-09-30 18:16:04 +0530

    Add project files.

---
+++ OrdoBooksWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OrdoBooksWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
