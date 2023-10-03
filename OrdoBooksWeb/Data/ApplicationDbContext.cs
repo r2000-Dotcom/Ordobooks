605651891b1e08413bc16e1b83878017909073db
Rupak Shukla <rupakshukla4@gmail.com>
2023-10-04 00:45:42 +0530

    Configure the project and add a services and buget packages connected the db to the project#

---
+++ OrdoBooksWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrdoBooksWeb.Models;
+
+namespace OrdoBooksWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+                
+        }
+      public DbSet<BookCategory> BookCategories { get; set; }
+
+
+    }
+}
---
+++ OrdoBooksWeb/Models/BookCategory.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrdoBooksWeb.Models
+{
+    public class BookCategory
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+
+
+
+
+    }
+}
